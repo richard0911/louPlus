8640ce1f3d02f8995e959e5feaf4d65c10e36b56
Richard.L <richard0911@github.com>
2017-12-15 00:50:16 +0800

    update

--- taxRate/calculator.py
+++ taxRate/calculator.py
@@ -3,65 +3,112 @@ import sys
 import os.path
 
 
-
-
-class getinfo():
+class Info:
     def __init__(self):
-        
+        self.__salaryl = {}
+        self.__configl = {}
+
+    @property
+    def salaryl(self):
+        return self.__salaryl
+
+    @salaryl.setter
+    def salaryl(self, list1):
+        self.__salaryl = list1
+
+    @property
+    def configl(self):
+        return self.__configl
+
+    @configl.setter
+    def configl(self, list2):
+        self.__configl = list2
 
 
-def getPara():
+def get_para():  # 从命令行参数获取输入信息
     paral = []
     dic = {}
     for i in sys.argv[1:]:
         paral.append(i)
-    
-    for i in len(paral):
+
+    for i in range(len(paral)):
         if paral[i] == '-c':
-            dic[cfg] = para[i+1]
+            dic['cfg'] = paral[i+1]
         elif paral[i] == '-d':
-            dic[icsv] = paral[i+1]
+            dic['icsv'] = paral[i+1]
         elif paral[i] == '-o':
-            dic[ocsv] = paral[i+1]
-        else:
-            print('Parameter Error')
-            sys.exit(1) 
-            
-    for i in dic.values():
-        if os.path.isfile(i):
-            continue
-        else:
-            print('File Error')
-            sys.exit(1)
+            dic['ocsv'] = paral[i+1]
+
     return dic
 
 
-def cal(price):
-    for wid in wages:
-        wage = wid[1]
-        insures = wage * 0.165
-        amount = wage - 3500 - insures
-        after = 0
-        if amount > 0:
-            if amount <= 1500:
-                amount = amount * 0.03 - 0
-            elif amount <= 4500:
-                amount = amount * 0.10 - 105
-            elif amount <= 9000:
-                amount = amount * 0.20 - 555
-            elif amount <= 35000:
-                amount = amount * 0.25 - 1005
-            elif amount <= 55000:
-                amount = amount * 0.30 - 2755
-            elif amount <= 80000:
-                amount = amount * 0.35 - 5505
+def read_file(filename):  # 读取文件
+    listt = []
+    with open(filename) as file:
+        for i in file:
+            if i == '\n':
+                break
+            if '=' in i:
+                opera = '='
             else:
-                amount = amount * 0.45 - 13505
-            after = wage - insures - amount
-        else:
-            after = wage - insures
-        print('{}:{:.2f}'.format(wid[0], after))
+                opera = ','
+            i, j = i.strip().split(opera)
+            listt.append([i.strip(), float(j.strip())])
+        dic = dict(listt)
+    return dic
 
+
+def write_file(filename, dic):  # 写入文件
+    with open(filename, 'w') as file:
+        for d in dic:
+            s = '{},{:.2f},{:.2f},{:.2f},{:.2f}\n'.format(d, dic[d][0]
+                                          , dic[d][1], dic[d][2]
+                                          , dic[d][3])
+            file.write(s)
+
+
+def cal(dc, ds):
+    dic = {}
+    JiShuL = dc['JiShuL']
+    JiShuH = dc['JiShuH']
+    insurespre = dc['YangLao'] + dc['YiLiao'] + dc['ShiYe'] + dc['GongShang'] + dc['ShengYu'] + dc['GongJiJin']
+
+    for i in ds:
+        salary = ds[i]
+        if salary > JiShuH:
+            insures = JiShuH * insurespre
+        elif salary < JiShuL:
+            insures = JiShuL * insurespre
+        else:
+            insures = salary * insurespre
+
+        tax = salary - insures
+
+        if tax <= 1500:
+            tax = tax * 0.03 - 0
+        elif tax <= 4500:
+            tax = tax * 0.10 - 105
+        elif tax <= 9000:
+            tax = tax * 0.20 - 555
+        elif tax <= 35000:
+            tax = tax * 0.25 - 1005
+        elif tax <= 55000:
+            tax = tax * 0.30 - 2755
+        elif tax <= 80000:
+            tax = tax * 0.35 - 5505
+        else:
+            tax = tax * 0.45 - 13505
+
+        after = salary - insures - tax
+        list1 = [salary, insures, tax, after]
+        dic[i] = list1
+
+    return dic
 
 if __name__ == '__main__':
-    dic1 = getpara()
+    dic1 = get_para()
+    cl = Info()
+    cl.salaryl = read_file(dic1['icsv'])
+    cl.configl = read_file(dic1['cfg'])
+    dic2 = cal(cl.configl, cl.salaryl)
+    write_file(dic1['ocsv'], dic2)
