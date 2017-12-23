0a156c39727b7a1965593421d2b4a332fef3c37e
Richard.L <richard0911@github.com>
2017-12-23 20:32:52 +0800

    update challenge8

--- week2/challenge8/news/app.py
+++ week2/challenge8/news/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
 from pymongo import MongoClient
-from pymongo.cursor import Cursor
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:xz910815@localhost/shiyanlou'
@@ -44,15 +45,14 @@ class File(db.Model):
     fid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     create_time = db.Column(db.DateTime)
-    category_id = db.Column(db.Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.cid'))
     content = db.Column(db.Text)
 
-    def __init__(self, fid, title, create_time, content, category_id):
-        self.fid = fid
+    def __init__(self, title, create_time, file, content):
         self.title = title
         self.create_time = create_time
+        self.category_id = file.cid
         self.content = content
-        self.category_id = category_id
 
     def __repr__(self):
         file_con = dict()
@@ -65,17 +65,26 @@ class File(db.Model):
         return file
 
     def add_tag(self, tag_name):
-        mdb.tag.insert({'id': self.fid, 'tag_name': tag_name})
+        tags = self.tags
+        if tag_name in tags:
+            pass
+        else:
+            mdb.tag.insert({'id': self.fid, 'tag_name': tag_name})
 
     def rmove_tag(self, tag_name):
-        mdb.tag.remove({'tag_name': tag_name})
+        tags = self.tags
+        if tags not in tags:
+            pass
+        else:
+            mdb.tag.remove({'tag_name': tag_name})
 
     @property
     def tags(self):
         tags = []
         result = mdb.tag.find({'id': self.fid})
         for doc in result:
-            tags.append(doc['tag'])
+            print(doc)
+            tags.append(doc['tag_name'])
         return tags
 
 
@@ -84,10 +93,27 @@ class Category(db.Model):
     cid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
-    def __init__(self, cid, name):
-        self.cid = cid
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
         return '{}'.format(self.name)
 
+
+def init_db():
+    db.create_all()
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File content- java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File content- python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+
+    file1.add_tag('tech')
+    file1.add_tag('linux')
+    file1.add_tag('java')
+    file2.add_tag('tech')
+    file2.add_tag('python')
