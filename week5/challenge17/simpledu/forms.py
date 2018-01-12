5f26aa3eac93eabb3ca99eea4c6b3ca7d6862438
ricard0911 <richard0911@github.com>
2018-01-12 21:52:31 +0800

    update challenge17

--- week5/challenge17/simpledu/forms.py
+++ week5/challenge17/simpledu/forms.py
@@ -1,14 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
+from simpledu.models import db, User
+from wtforms import ValidationError
 
 
 class RegisterFrom(FlaskForm):
-    username = StringField('用户名', validators=[Required(), Length(3, 24)])
-    email = StringField('邮箱', validators=[Required(), Email()])
-    password = PasswordField('密码', validators=[Required(), Length(6, 24)])
-    repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo(password)])
+    username = StringField('用户名', validators=[Required(), Length(3, 24, message='输入有误')])
+    email = StringField('邮箱', validators=[Required(), Email(message='输入有误')])
+    password = PasswordField('密码', validators=[Required(), Length(6, 24, message='输入有误')])
+    repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo(password, message='输入有误')])
     submit = SubmitField('提交')
+
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+
+    def valiodate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
 
 
 class LoginFrom(FlaskForm):
@@ -16,3 +35,12 @@ class LoginFrom(FlaskForm):
     password = PasswordField('密码', validators=[Required(), Length(6, 24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
+
+    def validate_email(self, field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱未注册')
+
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('密码错误')
--- week5/challenge17/simpledu/hendlers/front.py
+++ week5/challenge17/simpledu/hendlers/front.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
+from flask import url_for
 from ..models import Course
 from simpledu.forms import LoginFrom, RegisterFrom
+from flask import flash, redirect
+from flask_login import login_user
+from simpledu.models import User
 
 
 front = Blueprint('front', __name__)
@@ -12,13 +16,21 @@ def index():
     return render_template('index.html', courses=courses)
 
 
-@front.route('/login')
+@front.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginFrom()
+    if form.validate_on_submit():
+        user = User.qurey.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)
+        return redirect(url_for('.index'))
     return render_template('login.html', form=form)
 
 
-@front.route('/register')
+@front.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterFrom()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('注册成功，请登录！', 'success')
+        return redirect(url_for('/login'))
     return render_template('register.html', form=form)
