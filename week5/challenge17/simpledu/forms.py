9e70f14df87c756e94bb5637c0856e5dbbe970fd
ricard0911 <richard0911@github.com>
2018-01-13 20:02:21 +0800

    update challenge17

--- week5/challenge17/simpledu/forms.py
+++ week5/challenge17/simpledu/forms.py
@@ -6,11 +6,11 @@ from wtforms import ValidationError
 
 
 class RegisterFrom(FlaskForm):
-    username = StringField('用户名', validators=[Required(), Length(3, 24, message='输入有误')])
-    email = StringField('邮箱', validators=[Required(), Email(message='输入有误')])
-    password = PasswordField('密码', validators=[Required(), Length(6, 24, message='输入有误')])
-    repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password', message='输入有误')])
-    submit = SubmitField('提交')
+    username = StringField('Username', validators=[Required(), Length(3, 24, message='The Username should be 3 to 24 word')])
+    email = StringField('Email', validators=[Required(), Email(message='illegal Email ')])
+    password = PasswordField('Password', validators=[Required(), Length(6, 24, message='Input is wrong')])
+    repeat_password = PasswordField('Password Again', validators=[Required(), EqualTo('password', message='The two passwords are inconsistent')])
+    submit = SubmitField('Sibmit')
 
     def create_user(self):
         user = User()
@@ -23,24 +23,24 @@ class RegisterFrom(FlaskForm):
 
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
-            raise ValidationError('用户名已经存在')
+            raise ValidationError('The Username had exist')
 
-    def valiodate_email(self, field):
+    def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱已经存在')
+            raise ValidationError('The Email had exist')
 
 
 class LoginFrom(FlaskForm):
-    email = StringField('邮箱', validators=[Required(), Email()])
-    password = PasswordField('密码', validators=[Required(), Length(6, 24)])
-    remember_me = BooleanField('记住我')
-    submit = SubmitField('提交')
+    username = StringField('Username', validators=[Required(), Length(3, 24)])
+    password = PasswordField('PassWord', validators=[Required(), Length(6, 24)])
+    remember_me = BooleanField('Remenber me')
+    submit = SubmitField('Submit')
 
-    def validate_email(self, field):
-        if field.data and not User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱未注册')
+    def validate_username(self, field):
+        if field.data and not User.query.filter_by(username=field.data).first():
+            raise ValidationError('The account did not exists!')
 
     def validate_password(self, field):
-        user = User.query.filter_by(email=self.email.data).first()
+        user = User.query.filter_by(username=self.username.data).first()
         if user and not user.check_password(field.data):
-            raise ValidationError('密码错误')
+            raise ValidationError('Wrong password')
